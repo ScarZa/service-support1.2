f4072a864fc5bee61e266f045948472f74389d15
ScarZ <ScarZ@ScarZ>
2018-01-31 16:23:44 +0700

    develop : change flow room request 31/01/61

--- user/pre_order.php
+++ user/pre_order.php
@@ -91,7 +91,7 @@ if($_SESSION['ss_status']=='USER'){
             inner join emppersonal e on e.empno=ssc.empno_request
             inner JOIN work_history wh ON wh.empno=e.empno
             inner join department d on d.depId=wh.depid
-            where $code ssc.start_date between '$this_year-10-01' and '$next_year-09-30' and approve='Y' and (wh.dateEnd_w='0000-00-00' or ISNULL(wh.dateEnd_w))
+            where $code ssc.start_date between '$this_year-10-01' and '$next_year-09-30' and (wh.dateEnd_w='0000-00-00' or ISNULL(wh.dateEnd_w))
             order by ssc.conf_id desc";
     
     $q2="SELECT ssc . * , CONCAT( p1.pname, e.firstname,  ' ', e.lastname ) AS fullname, d1.depName AS dep, e.empno AS empno,
